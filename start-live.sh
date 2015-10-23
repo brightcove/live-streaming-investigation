11a96758498f0e0d070847b6dc7c700612262c26
David LaPalomento <dlapalomento@gmail.com>
2015-10-23 13:41:15 -0400

    Allow demo page to take a stream URL as a query param

--- start-live.sh
+++ start-live.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-ffmpeg -re -i gear4-full.mp4 -f segment -segment_time 2 -segment_list index.m3u8 -segment_format mpegts %05d.ts &
+http-server &
 open -a safari "http://localhost:8080/"
+ffmpeg -re -i gear4-full.mp4 -f segment -segment_time 2 -segment_list index.m3u8 -segment_format mpegts %05d.ts
